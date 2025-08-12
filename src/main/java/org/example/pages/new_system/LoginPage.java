13cb6632b28aca1cbdb3cbe858a1cbbbed719504
hoaithu0806 <hoaithux5k39@gmail.com>
2025-08-12 17:46:35 +0700

    update login page

--- src/main/java/org/example/pages/new_system/LoginPage.java
+++ src/main/java/org/example/pages/new_system/LoginPage.java
@@ -1,13 +1,13 @@
-package org.example.pages.old_system;
+package org.example.pages.new_system;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
     private final WebDriver driver;
-    private final By usernameField = By.id("txtPaid");
-    private final By passwordField = By.id("txtPapswd");
-    private final By loginBtn = By.id("btnOK");
+    private final By usernameField = By.name("username");
+    private final By passwordField = By.name("password");
+    private final By loginBtn = By.xpath("//button[@tabindex='3']");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
